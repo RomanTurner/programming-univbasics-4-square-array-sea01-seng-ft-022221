31a8b46123fcceffee7c71c0e56488129c53d93d
Roman Turner <rn.turner@live.com>
2021-01-19 05:04:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = []
  
   array.length.times do |index|
-  new_array.push(array[index] * [index])
+  new_array.push(array[index] * index)
   p new_array
 end
 end 
